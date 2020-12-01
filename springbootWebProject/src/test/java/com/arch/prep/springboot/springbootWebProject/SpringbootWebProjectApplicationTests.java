40be93a571b5b2212e254eaf6d0e22dd0d3c9ae5
haren <haren@host.docker.internal>
2020-11-30 22:05:54 -0600

    Spring Boot Initial Checkin

---
+++ springbootWebProject/src/test/java/com/arch/prep/springboot/springbootWebProject/SpringbootWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arch.prep.springboot.springbootWebProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
